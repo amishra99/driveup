d1289ccabc6c48a87cdb2b0d0e05d108db49ec45
amishra99 <53838847+amishra99@users.noreply.github.com>
2025-03-31 12:52:51 +0530

    Update drivebot.tsx

--- src/pages/drivebot.tsx
+++ src/pages/drivebot.tsx
@@ -63,7 +63,12 @@ import Head from "next/head";
 
 const Lottie = dynamic(() => import("react-lottie-player"), { ssr: false });
 
-const Sidebar = ({ isOpen, toggleSidebar }) => {
+type SidebarProps = {
+  isOpen: boolean;
+  toggleSidebar: () => void;
+};
+
+const Sidebar = ({ isOpen, toggleSidebar }: SidebarProps) => {
   const [user, setUser] = useState<any>(null);
   const [showModal, setShowModal] = useState(false);
   const [name, setName] = useState("");
